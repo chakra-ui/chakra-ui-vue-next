b840a5e3655f3036ca4119f4fb03e7b58497600e
codebender828 <jbakebwa@163.com>
2021-01-23 22:43:04 +0800

    chore: track playground generated files

---
+++ playground/src/.generated/imports.js
@@ -0,0 +1,14 @@
+import Component_1 from "../components/Home.vue";
+import Component_2 from '@chakra-ui/c-alert/examples/base-alert.vue'
+import Component_3 from '@chakra-ui/c-box/examples/base-box.vue'
+import Component_4 from '@chakra-ui/c-box/examples/box-with-chakra-directive.vue'
+import Component_5 from '@chakra-ui/c-button/examples/base-button.vue'
+import Component_6 from "../components/Home.vue";
+
+export default {
+  "../components/Home.vue": Component_6,
+  "@chakra-ui/c-alert/examples/base-alert.vue": Component_2,
+  "@chakra-ui/c-box/examples/base-box.vue": Component_3,
+  "@chakra-ui/c-box/examples/box-with-chakra-directive.vue": Component_4,
+  "@chakra-ui/c-button/examples/base-button.vue": Component_5
+}
---
+++ playground/src/.generated/resolver.js
@@ -0,0 +1,13 @@
+/* Package components resolver only used in development mode */
+  export default {
+  '@chakra-ui/c-accordion': '/Users/jonathanbakebwa/Github/chakra-ui-vue-next/packages/c-accordion/src/index.ts',
+  '@chakra-ui/c-alert': '/Users/jonathanbakebwa/Github/chakra-ui-vue-next/packages/c-alert/src/index.ts',
+  '@chakra-ui/c-box': '/Users/jonathanbakebwa/Github/chakra-ui-vue-next/packages/c-box/src/index.ts',
+  '@chakra-ui/c-button': '/Users/jonathanbakebwa/Github/chakra-ui-vue-next/packages/c-button/src/index.ts',
+  '@chakra-ui/c-theme-provider': '/Users/jonathanbakebwa/Github/chakra-ui-vue-next/packages/c-theme-provider/src/index.ts',
+  '@chakra-ui/styled-system': '/Users/jonathanbakebwa/Github/chakra-ui-vue-next/packages/styled-system/src/index.ts',
+  '@chakra-ui/system': '/Users/jonathanbakebwa/Github/chakra-ui-vue-next/packages/system/src/index.ts',
+  '@chakra-ui/theme': '/Users/jonathanbakebwa/Github/chakra-ui-vue-next/packages/theme/src/index.ts',
+  '@chakra-ui/theme-tools': '/Users/jonathanbakebwa/Github/chakra-ui-vue-next/packages/theme-tools/src/index.ts',
+  '@chakra-ui/utils': '/Users/jonathanbakebwa/Github/chakra-ui-vue-next/packages/utils/src/index.ts'
+}
