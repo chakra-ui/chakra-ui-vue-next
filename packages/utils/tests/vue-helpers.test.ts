3f9c09f652d5e93dc6ad9414261eec12693efbf5
Mesut Koca <imesutkoca@gmail.com>
2021-02-07 07:38:51 +0300

    chore(vue-utils): change import

--- packages/utils/tests/vue-helpers.test.ts
+++ packages/utils/tests/vue-helpers.test.ts
@@ -1,6 +1,6 @@
 import { createContext } from '@chakra-ui/vue-utils/src'
 import { defineComponent, h } from 'vue'
-import { render } from '../../test-utils/src'
+import { render } from '@chakra-ui/vue-test-utils/src'
 
 interface ExampleContext {
   example: string
