711b03456da13b7fcb01b002ad7ab9e3b20bdb71
codebender828 <jbakebwa@163.com>
2021-02-19 02:07:10 +0800

    chore(icon-button): added story for icon button

---
+++ packages/c-button/examples/as-icon-button.vue
@@ -0,0 +1,7 @@
+<template>
+  <chakra.p mt="3" font-weight="bold">IconButton</chakra.p>
+  <c-icon-button icon="star" mr="3" size="xs" color-scheme="teal" />
+  <c-icon-button icon="star" mr="3" size="sm" color-scheme="teal" />
+  <c-icon-button icon="star" mr="3" size="md" color-scheme="teal" />
+  <c-icon-button icon="star" mr="3" size="lg" color-scheme="teal" />
+</template>
--- packages/c-button/examples/with-button-icon.vue
+++ packages/c-button/examples/with-button-icon.vue
@@ -8,10 +8,5 @@
     <c-button mr="3" right-icon="star" color-scheme="blue">
       Base button
     </c-button>
-    <chakra.p mt="3" font-weight="bold">IconButton</chakra.p>
-    <c-icon-button icon="star" mr="3" size="xs" color-scheme="teal" />
-    <c-icon-button icon="star" mr="3" size="sm" color-scheme="teal" />
-    <c-icon-button icon="star" mr="3" size="md" color-scheme="teal" />
-    <c-icon-button icon="star" mr="3" size="lg" color-scheme="teal" />
   </div>
 </template>
