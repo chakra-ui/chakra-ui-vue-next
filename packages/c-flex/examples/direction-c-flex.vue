f1adb685828f90b87bac8599e39b447c355e664e
carwack <sybren.willemot@gmail.com>
2021-02-22 19:40:57 +0100

    feat(component): fix example templates for c flex

--- packages/c-flex/examples/direction-c-flex.vue
+++ packages/c-flex/examples/direction-c-flex.vue
@@ -1,14 +1,16 @@
 <template>
-  <chakra.h2>Row (default)</chakra.h2>
-  <c-flex row justify-content="space-between">
-    <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
-  </c-flex>
-  <chakra.h2>Column</chakra.h2>
-  <c-flex column justify-content="space-between">
-    <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
-  </c-flex>
+  <div>
+    <chakra.h2>Row (default)</chakra.h2>
+    <c-flex row justify-content="space-between">
+      <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
+      <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
+      <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
+    </c-flex>
+    <chakra.h2>Column</chakra.h2>
+    <c-flex column justify-content="space-between">
+      <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
+      <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
+      <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
+    </c-flex>
+  </div>
 </template>
--- packages/c-flex/examples/reverse-c-flex.vue
+++ packages/c-flex/examples/reverse-c-flex.vue
@@ -1,20 +1,22 @@
 <template>
-  <chakra.h2>Reverse (row default)</chakra.h2>
-  <c-flex reverse justify-content="space-between">
-    <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
-  </c-flex>
-  <chakra.h2>Reverse Row</chakra.h2>
-  <c-flex row reverse justify-content="space-between">
-    <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
-  </c-flex>
-  <chakra.h2>Reverse Column</chakra.h2>
-  <c-flex column reverse justify-content="space-between">
-    <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
-  </c-flex>
+  <div>
+    <chakra.h2>Reverse (row default)</chakra.h2>
+    <c-flex reverse justify-content="space-between">
+      <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
+      <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
+      <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
+    </c-flex>
+    <chakra.h2>Reverse Row</chakra.h2>
+    <c-flex row reverse justify-content="space-between">
+      <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
+      <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
+      <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
+    </c-flex>
+    <chakra.h2>Reverse Column</chakra.h2>
+    <c-flex column reverse justify-content="space-between">
+      <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
+      <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
+      <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
+    </c-flex>
+  </div>
 </template>
