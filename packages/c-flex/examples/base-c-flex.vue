af0be7126c5da9042b71e44cbf4f20abf7ac0877
carwack <sybren.willemot@gmail.com>
2021-02-22 19:54:51 +0100

    feat(component): restyle the examples of cflex

--- packages/c-flex/examples/base-c-flex.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <c-flex row justify-content="space-between">
-    <chakra.div h="20px" w="50px" bg="red.500">div 1</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 2</chakra.div>
-    <chakra.div h="20px" w="50px" bg="red.500">div 3</chakra.div>
-  </c-flex>
-</template>
---
+++ packages/c-flex/examples/base-flex.vue
@@ -0,0 +1,13 @@
+<template>
+  <c-flex row justify-content="space-between">
+    <chakra.div rounded="md" w="12" h="12" shadow="inner" mr="3" bg="green.500">
+      div 1
+    </chakra.div>
+    <chakra.div rounded="md" w="12" h="12" shadow="inner" mr="3" bg="blue.500">
+      div 2
+    </chakra.div>
+    <chakra.div rounded="md" w="12" h="12" shadow="inner" mr="3" bg="red.500">
+      div 3
+    </chakra.div>
+  </c-flex>
+</template>
