a80a4324fe1b72dd83b29d8bd95b1b87e7f965b2
carwack <sybren.willemot@gmail.com>
2021-02-25 15:05:18 +0100

    feat(test): add test for cflex component

--- packages/c-flex/examples/base-flex.vue
+++ packages/c-flex/examples/base-flex.vue
@@ -10,20 +10,4 @@
       <chakra.p bg="tomato" color="white"> Box 3 </chakra.p>
     </chakra.div>
   </c-flex>
-  <!-- <c-flex
-    direction="column"
-    justify="space-between"
-    :align="{ sm: 'flex-start', md: 'flex-end' }"
-    wrap="wrap"
-  >
-    <chakra.div rounded="md" w="12" h="12" shadow="inner" bg="green.500">
-      div 1
-    </chakra.div>
-    <chakra.div rounded="md" w="12" h="12" shadow="inner" bg="blue.500">
-      div 2
-    </chakra.div>
-    <chakra.div rounded="md" w="12" h="12" shadow="inner" bg="red.500">
-      div 3
-    </chakra.div>
-  </c-flex> -->
 </template>
--- packages/c-flex/tests/c-flex.test.ts
+++ packages/c-flex/tests/c-flex.test.ts
@@ -1,7 +1,18 @@
-import { render } from '../../test-utils/src'
 import { CFlex } from '../src'
+import { render, testA11y } from '../../test-utils/src'
+
+const renderComponent = (props?: any) =>
+  render({
+    components: { CFlex },
+    template: '<c-flex>Flex box !</c-flex>',
+    ...props,
+  })
 
 it('should render properly', () => {
-  const { asFragment } = render(CFlex)
+  const { asFragment } = renderComponent()
   expect(asFragment()).toMatchSnapshot()
+})
+
+it('should have no a11y violations', async () => {
+  await testA11y(renderComponent())
 })
