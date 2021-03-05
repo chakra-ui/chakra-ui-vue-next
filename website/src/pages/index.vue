bc517ec87333e113a6a87bef9eb5980fb5517875
carwack <sybren.willemot@gmail.com>
2021-03-05 10:13:56 +0100

    feat: add chakra ui vue logo

--- website/src/pages/index.vue
+++ website/src/pages/index.vue
@@ -1,3 +1,16 @@
 <template>
   <h1>Index</h1>
+  <ChakraUILogo />
 </template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import ChakraUILogo from '../components/logo.vue'
+
+export default defineComponent({
+  name: 'Index',
+  components: {
+    ChakraUILogo,
+  },
+})
+</script>
