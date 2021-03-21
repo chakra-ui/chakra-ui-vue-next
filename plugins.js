caa6bbddd4821ae3df642c208f3ab9c687b7e9e9
Jessica Sachs <jess@jessicasachs.io>
2021-03-21 01:42:20 -0400

    test: broken cypress + vite

---
+++ plugins.js
@@ -0,0 +1,8 @@
+const { startDevServer } = require('@cypress/vite-dev-server')
+
+module.exports = (on, config) => {
+  on('dev-server:start', (options) => {
+    return startDevServer({ options, viteConfig: require('./vite.config') })
+  })
+  return config
+}
