cf2008c77effc3f331b83d0324ed86c9958ae47b
codebender828 <jbakebwa@163.com>
2021-03-25 03:49:48 +0800

    test(colomode): add colormode tests

--- packages/c-color-mode/tests/c-color-mode.test.ts
+++ packages/c-color-mode/tests/c-color-mode.test.ts
@@ -1,6 +1,34 @@
-import { CColorMode } from '../'
+import { defineComponent, nextTick } from 'vue'
+import { render, userEvent } from '../../test-utils/src'
+import { useColorMode } from '../src'
 
+const renderComponent = (props?: any) => {
+  const base = defineComponent({
+    template: `
+      <div>
+        <div data-testid="colormode"> {{ colorMode }} </div>
+        <button data-testid="toggle" @click="toggleColorMode">Toggle color mode</button>
+      </div>
+    `,
+    setup() {
+      const { colorMode, toggleColorMode } = useColorMode()
+      return {
+        colorMode,
+        toggleColorMode,
+      }
+    },
+    ...props,
+  })
+  return render(base)
+}
 
-it('should be truthy', () => {
-  expect(1).toBe(1)
+it('should toggle colormode', async () => {
+  const { getByTestId, asFragment } = renderComponent()
+  expect(getByTestId('colormode').textContent).toBe('light')
+
+  await userEvent.click(getByTestId('toggle'))
+  await nextTick()
+  expect(getByTestId('colormode').textContent).toBe('dark')
+
+  expect(asFragment()).toMatchSnapshot()
 })
