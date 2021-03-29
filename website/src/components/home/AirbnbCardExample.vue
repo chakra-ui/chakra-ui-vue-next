9bbfc4b0a49e73bce98376b6903cbe8541f06261
carwack <sybren.willemot@gmail.com>
2021-03-29 11:06:02 +0200

    feat: wip add example airbnb with css scroll

---
+++ website/src/components/home/AirbnbCardExample.vue
@@ -0,0 +1,39 @@
+<template>
+  <chakra.div w="100%" shadow="md" p="5" bg="white">
+    <chakra.img rounded="md" w="100%" src="https://bit.ly/2k1H1t6" />
+    <CFlex align="baseline" mt="2">
+      <CBadge variant-color="pink"> Plus </CBadge>
+      <chakra.p
+        ml="2"
+        text-transform="uppercase"
+        font-size="sm"
+        font-weight="bold"
+        color="pink.800"
+      >
+        Verified &bull; Cape Town
+      </chakra.p>
+    </CFlex>
+    <chakra.p mt="2" font-size="xl" font-weight="semibold" line-height="short">
+      Modern, Chic Penthouse with Mountain, City & Sea Views
+    </chakra.p>
+    <chakra.p mt="2"> $119/night </chakra.p>
+    <chakra.p mt="2" align="center">
+      <CIcon name="star" color="orange.400" />
+      <chakra.p ml="1" font-size="sm"> <b>4.84</b> (190) </chakra.p>
+    </chakra.p>
+  </chakra.div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { CBadge, CFlex, CIcon } from '@chakra-ui/vue-next'
+
+export default defineComponent({
+  name: 'Footer',
+  components: {
+    CBadge,
+    CFlex,
+    CIcon,
+  },
+})
+</script>
--- website/src/pages/index.vue
+++ website/src/pages/index.vue
@@ -7,17 +7,41 @@
     bg="vue.50"
   >
     <Hero />
+    <!-- width from 40vw to 300px & code to flex 1 -->
+    <chakra.div
+      w="100%"
+      min-h="500px"
+      mt="10"
+      pos="relative"
+      display="flex"
+      :flex-direction="{ base: 'column', md: 'row' }"
+    >
+      <chakra.div w="calc(100% - 348px)" pl="12" pr="4">
+        <chakra.div h="100%" w="100%" bg="black" shadow="md" />
+      </chakra.div>
+      <div class="example" :class="{ small: scrolled }">
+        <chakra.div
+          :w="{ base: '100%', md: '100%' }"
+          top="-70%"
+          right="5%"
+          mr="12"
+        >
+          <AirbnbCardExample />
+        </chakra.div>
+      </div>
+    </chakra.div>
     <PilersHero />
     <Footer />
   </chakra.main>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 import Hero from '../components/home/Hero.vue'
 import PilersHero from '../components/home/PilersHero.vue'
 import Navbar from '../components/Navbar.vue'
 import Footer from '../components/home/Footer.vue'
+import AirbnbCardExample from '../components/home/AirbnbCardExample.vue'
 
 export default defineComponent({
   name: 'Index',
@@ -26,6 +50,51 @@ export default defineComponent({
     PilersHero,
     Navbar,
     Footer,
+    AirbnbCardExample,
+  },
+  created() {
+    if (!!window) {
+      window.addEventListener('scroll', this.handleScroll)
+    }
+  },
+  unmounted() {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  setup() {
+    const scrolled = ref(false)
+
+    function handleScroll() {
+      if (window.scrollY > 100) {
+        scrolled.value = true
+      } else {
+        scrolled.value = false
+      }
+    }
+    return {
+      handleScroll,
+      scrolled,
+    }
   },
 })
 </script>
+
+<style scoped>
+.example {
+  width: 40vw;
+  top: -85%;
+  right: 5%;
+  position: absolute;
+  transition: all 0.5s ease-in-out;
+}
+
+.example.small {
+  width: 300px;
+  margin-right: 48px;
+  /* position: initial; */
+  top: 0;
+  right: 0;
+  /* transition: width 1s ease-in-out; */
+  transition: all 1s ease-in-out;
+  /* transition: right 0.5s ease-in-out; */
+}
+</style>
