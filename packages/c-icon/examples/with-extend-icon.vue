5b537a551a842d4adb743c8b5e14bdbd37563349
carwack <sybren.willemot@gmail.com>
2021-04-01 16:28:22 +0200

    feat: add example of extend icon

---
+++ packages/c-icon/examples/with-extend-icon.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <c-icon size="10" name="discord" />
+  </div>
+</template>
+
+<script lang="ts">
+import { CIcon } from '@chakra-ui/c-icon'
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  components: { CIcon },
+})
+</script>
