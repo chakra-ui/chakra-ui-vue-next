bf2de188d2c530a7f1b39f3cc3f75c0809186617
carwack <sybren.willemot@gmail.com>
2021-04-05 18:44:55 +0200

    feat: add discord hero

---
+++ website/src/components/home/DiscordHero.vue
@@ -0,0 +1,73 @@
+<template>
+  <CFlex
+    as="section"
+    :flex-dir="['column', 'column', 'row']"
+    align="center"
+    justify="space-between"
+    font-family="body"
+    color="white"
+    bg="discord.accessible"
+    :h="['auto', 40]"
+    :px="[4, 10, 12]"
+  >
+    <CFlex
+      flex-dir="row"
+      py="10"
+      :mr="{ base: '0', sm: '10' }"
+      :w="{ base: '100%', md: 'auto' }"
+    >
+      <CIcon name="discord" size="48px" mr="5" />
+      <CBox>
+        <chakra.h2 font-size="md" font-weight="bold" lineHeight="1.2" mb="1">
+          Connect with the Chakra community
+        </chakra.h2>
+        <chakra.p>
+          Feel free to ask questions, report issues, and meet new people.
+        </chakra.p>
+      </CBox>
+    </CFlex>
+    <CBox
+      :w="{ base: 0, md: '10' }"
+      h="100%"
+      bg="vue.50"
+      transform="skewX(20deg)"
+    />
+    <CFlex
+      flex-dir="row"
+      py="10"
+      h="100%"
+      :w="{ base: '100%', md: 'auto' }"
+      align="center"
+    >
+      <CButton
+        as="a"
+        text-decoration="none"
+        aria-label="Join Discord channel"
+        href="https://discord.gg/sq2Kp6x"
+        target="_blank"
+        rel="noopener noreferrer"
+        size="lg"
+        color="black"
+        bg="white"
+        shadow="lg"
+      >
+        Join the #Chakra Discord!
+      </CButton>
+    </CFlex>
+  </CFlex>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { CFlex, CBox, CButton, CIcon } from '@chakra-ui/vue-next'
+
+export default defineComponent({
+  name: 'DiscordHero',
+  components: {
+    CFlex,
+    CBox,
+    CButton,
+    CIcon,
+  },
+})
+</script>
