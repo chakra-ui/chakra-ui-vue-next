cbc55a06f372822dd41577f9bb0066e4b34e2f4d
Mesut Koca <imesutkoca@gmail.com>
2021-04-10 17:40:00 +0300

    test(layout): refactor tests

--- packages/layout/tests/layout.test.ts
+++ packages/layout/tests/layout.test.ts
@@ -1,37 +1,38 @@
 import { CLink, CBadge } from '../src'
-import { render, testA11y } from '../../test-utils/src'
+import { render, testA11y, TestRenderProps } from '../../test-utils/src'
 
-const renderComponent = (props?: any) =>
-  render({
-    components: { CLink },
-    template: `<c-link>CLink</c-link>
-    <c-link is-external>CLink external</c-link>
-    `,
-    ...props,
+describe('<CLink />', () => {
+  const renderComponent = () =>
+    render({
+      components: { CLink },
+      template: `<c-link>CLink</c-link>
+      <c-link is-external>CLink external</c-link>
+      `,
+    })
+
+  it('should render properly', () => {
+    const { asFragment } = renderComponent()
+    expect(asFragment()).toMatchSnapshot()
   })
 
-it('should render properly', () => {
-  const { asFragment } = renderComponent()
-  expect(asFragment()).toMatchSnapshot()
-})
-
-it('should have no a11y violations', async () => {
-  await testA11y(renderComponent())
+  it('should have no a11y violations', async () => {
+    await testA11y(renderComponent())
+  })
 })
 
 describe('<CBadge />', () => {
-  const renderBadgeComponent = () =>
+  const renderComponent = () =>
     render({
       components: { CBadge },
       template: `<CBadge>this is a badge</CBadge>`,
     })
 
   test('should render properly', async () => {
-    const { asFragment } = renderBadgeComponent()
+    const { asFragment } = renderComponent()
     expect(asFragment()).toMatchSnapshot()
   })
 
   test('passes a11y test', async () => {
-    await testA11y(renderBadgeComponent())
+    await testA11y(renderComponent())
   })
 })
