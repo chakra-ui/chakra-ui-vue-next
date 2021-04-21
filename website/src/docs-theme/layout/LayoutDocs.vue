ad0e13520e2c5f5288728f37f8ae47f00a343430
carwack <sybren.willemot@gmail.com>
2021-04-21 19:00:00 +0200

    feat: add basic layout with navbar for docs

--- website/src/docs-theme/layout/LayoutDocs.vue
+++ website/src/docs-theme/layout/LayoutDocs.vue
@@ -1,9 +1,20 @@
 <template>
-  <chakra.div border="1px solid red" max-w="500px"><slot /></chakra.div>
+  <CFlex max-h="calc(100vh - 60px)">
+    <CBox w="20vw" />
+    <CBox
+      id="main-content"
+      ref="docContainer"
+      as="section"
+      w="100%"
+      height="calc(100vh - 60px)"
+      overflow-y="scroll"
+      :py="[5, 20]"
+      :px="[10, 10, 20, '14rem']"
+      font-family="body"
+    >
+      <slot />
+    </CBox>
+  </CFlex>
 </template>
 
-<script>
-export default {}
-</script>
-
-<style></style>
+<script setup lang="ts"></script>
--- website/src/docs-theme/layout/MdxLayoutWrapper.vue
+++ website/src/docs-theme/layout/MdxLayoutWrapper.vue
@@ -1,7 +1,13 @@
 <template>
-  <chakra.div :min-h="['auto', 'auto', '100vh']" w="100%" pt="60px" bg="vue.50">
-    <app-header :frontmatter="frontmatter" />
-    <layout-docs style="border: 1px solid green" v-if="isDocs">
+  <c-reset />
+  <Navbar />
+  <chakra.main
+    :min-h="['auto', 'auto', '100vh']"
+    w="100%"
+    pt="60px"
+    bg="vue.50"
+  >
+    <layout-docs v-if="isDocs">
       <slot />
     </layout-docs>
     <layout-blog style="border: 1px solid red" v-else-if="isBlog">
@@ -10,13 +16,15 @@
     <layout-default v-else class="default">
       <slot />
     </layout-default>
-  </chakra.div>
+  </chakra.main>
 </template>
 
 <script setup lang="ts">
 import { computed, defineProps } from 'vue'
 import { useRoute } from 'vue-router'
 import { useHead } from '@vueuse/head'
+import Navbar from '@/components/Navbar.vue'
+
 const { path } = useRoute()
 const rootPath = '/'
 const blogPath = `${rootPath}blog`
