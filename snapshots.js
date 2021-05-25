69b31ae84c5db3e5eae3dbb7b9697f21a91da22c
codebender828 <jbakebwa@163.com>
2021-05-25 11:18:52 +0800

    fix(cypress): cypress test types all rnning

--- snapshots.js
+++ snapshots.js
@@ -1,10 +1,10 @@
 module.exports = {
-  "__version": "7.2.0",
-  "<Portal />": {
-    "Portal Examples": {
-      "renders SimplePortal successfully": {
-        "1": "<div data-v-app=\"\">\n  <div class=\"css-16m79av\">\n    <section class=\"css-0\"> Open Vue Dev Tools to inspect page for portal\n      elements. <span class=\"css-0\"></span></section>\n    <main class=\"css-0\">\n      <!--teleport start-->\n      <!--teleport end-->\n    </main>\n  </div>\n  <!---->\n</div>"
-      }
-    }
-  }
+  __version: '7.2.0',
+  '<Portal />': {
+    'Portal Examples': {
+      'renders SimplePortal successfully': {
+        1: '<div data-v-app="">\n  <div class="css-16m79av">\n    <section class="css-0"> Open Vue Dev Tools to inspect page for portal\n      elements. <span class="css-0"></span></section>\n    <main class="css-0">\n      <!--teleport start-->\n      <!--teleport end-->\n    </main>\n  </div>\n  <!---->\n</div>',
+      },
+    },
+  },
 }
