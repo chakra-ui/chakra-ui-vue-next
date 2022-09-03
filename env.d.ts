fe54713e196cb5b8e63b115115163959958852d6
codebender828 <jbakebwa@163.com>
2022-09-03 22:30:38 +0800

    chore: types fixed

--- env.d.ts
+++ env.d.ts
@@ -1,10 +1,14 @@
 import type { HTMLAttributes, VNode } from "vue"
 import type { ChakraProps } from "@chakra-ui/vue-system"
+import { DefineComponent } from "vue"
+import { ChakraFactoryProps, ComponentWithProps } from "@chakra-ui/vue-system"
 
 declare global {
-  namespace h.JSX {
+  namespace JSX {
     interface Element extends VNode {}
-    interface ElementClass {
+    interface ElementClass
+      extends DefineComponent<ChakraFactoryProps>,
+        ComponentWithProps<ChakraFactoryProps> {
       $props: {}
     }
     interface ElementAttributesProperty {
@@ -15,4 +19,10 @@ declare global {
       extends Omit<HTMLAttributes, "color">,
         ChakraProps {}
   }
+}
+
+declare module "vue" {
+  export type JSXComponent<Props = any> =
+    | { new (): ComponentPublicInstance<Props> }
+    | FunctionalComponent<Props>
 }
