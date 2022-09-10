df6ee088fddf85d7265806c11b3d44ea7f704223
codebender828 <jbakebwa@163.com>
2022-09-10 16:06:54 +0800

    test: refactor test snapshots

--- packages/utils/src/generate-id.ts
+++ packages/utils/src/generate-id.ts
@@ -1,8 +1,10 @@
 export function genId(size = 3) {
   let uuid = ""
-  const dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+  const dictionary =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
   for (let i = 0; i < size; i++) {
     uuid += dictionary.charAt(Math.floor(Math.random() * dictionary.length))
   }
+  if (process.env.NODE_ENV === "test") return "GENERATED_TEST_ID"
   return uuid
 }
