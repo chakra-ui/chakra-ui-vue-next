1477492ba2a810b1efa7a6cd2f5975d16ab23016
tylerapfledderer <tyler.pfledderer@gmail.com>
2022-09-28 23:24:51 -0400

    docs(c-skip-nav): add WebAIM link

--- packages/c-skip-nav/src/c-skip-nav.ts
+++ packages/c-skip-nav/src/c-skip-nav.ts
@@ -5,7 +5,7 @@
  *
  * @see Docs     https://next.vue.chakra-ui.com/c-skip-nav
  * @see Source   https://github.com/chakra-ui/chakra-ui-vue-next/blob/master/packages/c-skip-nav/src/c-skip-nav/c-skip-nav.ts
- * @see WAI-ARIA https://www.w3.org/TR/wai-aria-practices-1.2
+ * @see WebAIM   https://webaim.org/techniques/skipnav/
  */
 
 import { h, defineComponent } from "vue"
