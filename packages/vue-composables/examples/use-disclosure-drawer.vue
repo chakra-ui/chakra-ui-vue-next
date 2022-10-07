7ea7ef98c628bba09e50832753e4980f0d35107d
tylerapfledderer <tyler.pfledderer@gmail.com>
2022-10-06 21:32:36 -0400

    feat(use-disclosure): add playground examples

---
+++ packages/vue-composables/examples/use-disclosure-drawer.vue
@@ -0,0 +1,20 @@
+<template>
+  <c-button @click="onOpen">Open Drawer</c-button>
+  <c-drawer placement="right" v-model="isOpen">
+    <c-drawer-overlay />
+    <c-drawer-content>
+      <c-drawer-header borderBottomWidth="1px">Basic Drawer</c-drawer-header>
+      <c-drawer-close-button @click="onClose" />
+      <c-drawer-body>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </c-drawer-body>
+    </c-drawer-content>
+  </c-drawer>
+</template>
+<script setup>
+import { useDisclosure } from "../src/use-disclosure"
+
+const { onOpen, onClose, isOpen } = useDisclosure()
+</script>
