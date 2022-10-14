a9ff641be8525c30d628575512f861fcbd9b2cfa
prazdevs <sachabouillez@gmail.com>
2022-10-15 01:36:40 +0200

    feat(close-button): move to tsx

--- packages/c-close-button/src/c-close-button.tsx
+++ packages/c-close-button/src/c-close-button.tsx
@@ -8,25 +8,20 @@ import {
 } from "@chakra-ui/vue-system"
 import { filterUndefined } from "@chakra-ui/utils"
 import { CIcon } from "@chakra-ui/c-icon"
+import styles from "@chakra-ui/vue-theme/src/styles"
 
 const CCloseIcon = defineComponent({
   setup(_, { attrs }) {
-    return () =>
-      h(
-        CIcon,
-        {
-          focusable: false,
-          "aria-hidden": true,
-          name: "close",
-          ...attrs,
-        },
-        () => [
-          h("path", {
-            fill: "currentColor",
-            d: "M.439,21.44a1.5,1.5,0,0,0,2.122,2.121L11.823,14.3a.25.25,0,0,1,.354,0l9.262,9.263a1.5,1.5,0,1,0,2.122-2.121L14.3,12.177a.25.25,0,0,1,0-.354l9.263-9.262A1.5,1.5,0,0,0,21.439.44L12.177,9.7a.25.25,0,0,1-.354,0L2.561.44A1.5,1.5,0,0,0,.439,2.561L9.7,11.823a.25.25,0,0,1,0,.354Z",
-          }),
-        ]
-      )
+    return () => (
+      <CIcon focusable="false" aria-hidden="true" name="close" {...attrs}>
+        {() => (
+          <path
+            fill="currentColor"
+            d="M.439,21.44a1.5,1.5,0,0,0,2.122,2.121L11.823,14.3a.25.25,0,0,1,.354,0l9.262,9.263a1.5,1.5,0,1,0,2.122-2.121L14.3,12.177a.25.25,0,0,1,0-.354l9.263-9.262A1.5,1.5,0,0,0,21.439.44L12.177,9.7a.25.25,0,0,1-.354,0L2.561.44A1.5,1.5,0,0,0,.439,2.561L9.7,11.823a.25.25,0,0,1,0,.354Z"
+          />
+        )}
+      </CIcon>
+    )
   },
 })
 
@@ -69,21 +64,18 @@ export const CCloseButton = defineComponent({
 
       const styles = useStyleConfig("CloseButton", themingProps)
 
-      return h(
-        chakra(props.as, {
-          label: "icon-button",
-          __css: {
-            ...baseStyles,
-            ...styles.value,
-          },
-        }),
-        {
-          type: "button",
-          disabled: props.isDisabled,
-          "aria-label": "Close",
-          ...attrs,
-        },
-        slots.default ? slots : () => [h(CCloseIcon)]
+      return (
+        <chakra.button
+          as={props.as}
+          type="button"
+          disabled={props.isDisabled}
+          aria-label="Close"
+          __label="icon-button"
+          __css={{ ...baseStyles, ...styles.value }}
+          {...attrs}
+        >
+          {slots.default ? slots : () => <CCloseIcon />}
+        </chakra.button>
       )
     }
   },
