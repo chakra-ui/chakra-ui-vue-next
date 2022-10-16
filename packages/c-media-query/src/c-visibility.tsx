f1e8ff5b9c3973fe6af433ce209a5dcd51f2a85c
tylerapfledderer <tyler.pfledderer@gmail.com>
2022-10-15 22:27:52 -0400

    feat(c-visibility): create component

---
+++ packages/c-media-query/src/c-visibility.tsx
@@ -0,0 +1,30 @@
+import { computed, defineComponent, PropType } from "vue"
+import { useMediaQuery } from "@vueuse/core"
+import { ComponentWithProps } from "@chakra-ui/vue-system"
+
+export interface CVisbilityProps {
+  /**
+   * Test
+   */
+  breakpoint: string
+  hide?: boolean
+}
+
+export const CVisibility: ComponentWithProps<CVisbilityProps> = defineComponent(
+  {
+    props: {
+      breakpoint: {
+        type: String as PropType<CVisbilityProps["breakpoint"]>,
+        required: true,
+      },
+      hide: Boolean as PropType<CVisbilityProps["hide"]>,
+    },
+    setup(props, { slots }) {
+      const show = useMediaQuery(props.breakpoint)
+
+      const isVisible = computed(() => (props.hide ? !show.value : show.value))
+
+      return () => (isVisible.value ? slots.default!() : null)
+    },
+  }
+)
