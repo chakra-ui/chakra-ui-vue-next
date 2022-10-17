d999a6c8298fbf7137408c5e8b26e04832e5e385
tylerapfledderer <tyler.pfledderer@gmail.com>
2022-10-16 20:22:17 -0400

    feat(c-media-query): create `useQuery` composable

---
+++ packages/c-media-query/src/use-query.ts
@@ -0,0 +1,30 @@
+import { useTheme } from "@chakra-ui/vue-system"
+import { computed } from "vue"
+
+const getBreakpoint = (theme: Record<string, any>, value: any) => {
+  return theme?.breakpoints?.[value] ?? value
+}
+
+export interface UseQueryProps {
+  breakpoint?: string
+  below?: string
+  above?: string
+}
+
+export function useQuery(props: UseQueryProps) {
+  const { breakpoint = "", below, above } = props
+
+  const theme = useTheme()
+  const bpBelow = getBreakpoint(theme, below)
+  const bpAbove = getBreakpoint(theme, above)
+
+  const query = computed(() => {
+    if (bpAbove) return `(min-width: ${bpAbove})`
+
+    if (bpBelow) return `(max-width: ${bpBelow})`
+
+    return breakpoint
+  })
+
+  return query
+}
