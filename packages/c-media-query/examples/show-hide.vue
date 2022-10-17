8ef90817d94b6fffd12f34c79755efdbeea2952d
tylerapfledderer <tyler.pfledderer@gmail.com>
2022-10-16 20:24:45 -0400

    feat(c-media-query): add example for show/hide components

---
+++ packages/c-media-query/examples/show-hide.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <c-show above="md">
+      <p>This is shown above the 'md' breakpoint</p>
+    </c-show>
+    <c-hide above="lg">
+      <p>This hides above the 'lg' breakpoint</p>
+    </c-hide>
+  </div>
+</template>
+<script setup lang="ts">
+import { CShow, CHide } from "../src/index"
+</script>
