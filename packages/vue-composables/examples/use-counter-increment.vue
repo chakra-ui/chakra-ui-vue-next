5b1a860994e5fc441cfeacf888645b13fabf7b3e
tylerapfledderer <tyler.pfledderer@gmail.com>
2022-10-25 09:34:31 -0400

    feat(use-counter): create `useCounter` composable with example

---
+++ packages/vue-composables/examples/use-counter-increment.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <pre>
+      {{
+        JSON.stringify({
+          value,
+          valueAsNumber,
+          isOutOfRange,
+        })
+      }}
+    </pre>
+    <br />
+    <button @click="increment()">Click Me to Increase!</button>
+    <br />
+    <button @click="decrement()">Click Me to Decrease!</button>
+    <br />
+    <button @click="update(4)">Update to 4!</button>
+    <br />
+    <input v-model="value" :style="{ background: 'transparent' }" />
+  </div>
+</template>
+<script setup lang="ts">
+import { useCounter } from "../index"
+
+const { increment, decrement, valueAsNumber, update, value, isOutOfRange } =
+  useCounter({
+    max: 10,
+    min: 0,
+    keepWithinRange: false,
+    precision: 4,
+    step: 0.5,
+  })
+</script>
