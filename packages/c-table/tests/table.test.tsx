0e0ebe9fca06dc2371b428c7e18d41a2356f1cd4
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-01-24 14:12:27 -0500

    test(c-table): add a11y test

---
+++ packages/c-table/tests/table.test.tsx
@@ -0,0 +1,56 @@
+import { render, testA11y } from "@chakra-ui/vue-test-utils"
+import {
+  CTable,
+  CTableCaption,
+  CThead,
+  CTbody,
+  CTr,
+  CTh,
+  CTd,
+  CTfoot,
+} from "../src"
+
+describe("Table", () => {
+  it("should pass a11y test", async () => {
+    const renderComponent = () =>
+      render({
+        components: {
+          CTable,
+          CTableCaption,
+          CThead,
+          CTr,
+          CTh,
+          CTbody,
+          CTfoot,
+        },
+        template: `
+      <c-table>
+      <c-table-caption>Imperial to metric conversion factors</c-table-caption>
+      <c-thead>
+        <c-tr>
+          <c-th>To convert</c-th>
+          <c-th>into</c-th>
+          <c-th isNumeric>multiply by</c-th>
+        </c-tr>
+      </c-thead>
+      <c-tbody>
+        <c-tr>
+          <c-td>inches</c-td>
+          <c-td>millimetres (mm)</c-td>
+          <c-td isNumeric>25.4</c-td>
+        </c-tr>
+      </c-tbody>
+      <c-tfoot>
+        <c-tr>
+          <Th>To convert</Th>
+          <Th>into</Th>
+          <Th isNumeric>multiply by</Th>
+        </c-tr>
+      </c-tfoot>
+    </c-table>
+      `,
+      })
+
+    await testA11y(renderComponent())
+  })
+})
