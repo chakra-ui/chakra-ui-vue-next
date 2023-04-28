18b44945853230a1636376fa09bc09a27ff04d44
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-04-28 14:21:53 -0400

    fix(vue/c-card): fix module handing and an export

--- packages/c-card/src/index.ts
+++ packages/c-card/src/index.ts
@@ -1,3 +1,3 @@
-export { CCard, CCardProps } from "./c-card"
+export { CCard, type CCardProps } from "./c-card"
 export { CCardBody, type CCardBodyProps } from "./c-card-body"
 export { CCardHeader, type CCardHeaderProps } from "./c-card-header"
