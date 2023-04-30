50f7f22e56b29c2f2fa24f99427dec172dc53309
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-04-29 20:41:31 -0400

    test(c-card): update snapshot test

--- packages/c-card/tests/c-card.test.ts
+++ packages/c-card/tests/c-card.test.ts
@@ -1,7 +1,19 @@
 import { render } from "../../test-utils/src"
-import { CCard } from "../src"
+import { CCard, CCardHeader, CCardBody } from "../src"
 
 it("should render properly", () => {
-  const { asFragment } = render(CCard)
+  const { asFragment } = render({
+    components: { CCard, CCardHeader, CCardBody },
+    template: `
+      <c-card>
+        <c-card-header>
+          Customer dashboard
+        </c-card-header>
+        <c-card-body>
+            View a summary of all your customers over the last month.
+        </c-card-body>
+      </c-card>
+    `,
+  })
   expect(asFragment()).toMatchSnapshot()
 })
