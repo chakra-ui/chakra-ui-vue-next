dae5d3dacd6f5608981d72e5e73f8e78928a828a
tylerapfledderer <tyler.pfledderer@gmail.com>
2023-04-29 20:39:46 -0400

    chore(c-card): fix type import in context

--- packages/c-card/src/card.context.ts
+++ packages/c-card/src/card.context.ts
@@ -1,4 +1,4 @@
 import { createStylesContext } from "@chakra-ui/vue-system"
-import type * as SS from "@chakra-ui/styled-system"
+import type * as SS from "@chakra-ui/vue-system"
 
 export const [CCardStylesProvider, useCardStyles] = createStylesContext("Card")
